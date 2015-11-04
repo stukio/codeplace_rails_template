6f1da4c6708caa23709128f323cb0362aea099af
Tiago Martins <tiagom87@gmail.com>
2015-11-04 14:28:41 +0000

    Generated and associated user to posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151104141746) do
+ActiveRecord::Schema.define(version: 20151104142352) do
+
+  create_table "posts", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
